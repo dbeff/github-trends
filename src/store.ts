24e4c9bd35863f31c3d5f6e24b1012134b50a133
Daniel Beff <daniel.beff@gmail.com>
2025-08-22 00:56:58 +0200

    Added useAppDispatch and useAppSelector

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import filtersReducer from "./store/filters/filtersSlice";
 import { useDispatch, useSelector } from "react-redux";
+import { githubApi } from "./store/github/githubApi";
 
 export const store = configureStore({
   reducer: {
     filters: filtersReducer,
+    [githubApi.reducerPath]: githubApi.reducer,
   },
+  middleware: (getDefault) => getDefault().concat(githubApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
-export const useAppSelector = useSelector.withTypes<RootState>()
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
+
